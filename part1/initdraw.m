c57469cc6520fddef548fd6fddfff10c1304b126
Ryan Brigden <rbrigden@cmu.edu>
2019-02-09 15:02:13 -0500

    Add plot 3d example

--- part1/initdraw.m
+++ part1/initdraw.m
@@ -1,4 +1,4 @@
-global h_axes link1 link2 link3 target
+% global h_axes link1 link2 link3 target
 
 scrsz = get(0,'ScreenSize');
 animation = figure(1);
@@ -10,11 +10,11 @@ color3 = [0 1 0];
 color4 = [1 0 1];
 
 set(animation,'name','My Robot','Position',[20 100 500 500]);
-h_axes = axes('Parent',animation,'Units','Pixels','Position',[0 0 500 500],'Ylim',[-1.0 1.0],'Xlim',[-1.0 1.0]);
+h_axes = axes('Parent',animation,'Units','Pixels','Position',[0 0 500 500],'Ylim',[-1.0 1.0],'Xlim',[-1.0 1.0], 'Zlim',[-1.0 1.0]);
+% 
+% target = line('Parent',h_axes,'Color',color4,'Visible','off','LineWidth',10);
+% link1 = line('Parent',h_axes,'Color',color1,'Visible','off','LineWidth',10);
+% link2 = line('Parent',h_axes,'Color',color2,'Visible','off','LineWidth',10);
+% link3 = line('Parent',h_axes,'Color',color3,'Visible','off','LineWidth',10);
 
-target = line('Parent',h_axes,'Color',color4,'Visible','off','LineWidth',10);
-link1 = line('Parent',h_axes,'Color',color1,'Visible','off','LineWidth',10);
-link2 = line('Parent',h_axes,'Color',color2,'Visible','off','LineWidth',10);
-link3 = line('Parent',h_axes,'Color',color3,'Visible','off','LineWidth',10);
-
-set(h_axes,'visible','on');
+% set(h_axes,'visible','on');
