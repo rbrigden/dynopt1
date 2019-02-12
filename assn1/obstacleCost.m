4b67a32522f4e0d58fcb45ad0c22c8eb2a825749
Marcus Greer <marcusgreer96@gmail.com>
2019-02-12 17:46:24 -0500

    basic obstacle cost function

---
+++ assn1/obstacleCost.m
@@ -0,0 +1,22 @@
+function [cost] = obstacleCost(linkPoints,obstacles)
+% obstacleCost:
+% given a matrix of obstables where each obstacle is a row of x y z radius (spheres)
+% get the distance to all link points (returned by the fk function)
+
+%TODO: make distance from middle of links instead of edges
+N = size(linkPoints,1);
+M = size(obstacles,1);
+
+obstaclePos = obstacles(:, 1:end-1);
+obstacleRad = obstacles(:, end);
+
+cost = zeros(N,M);
+for n = 1:N
+    for m = 1:M
+        dist = sqrt(sum((linkPoints(n,:) - obstaclePos(m, :)).^2));
+        cost(n,m) = dist - obstacleRad(m);
+    end
+end
+
+end
+
