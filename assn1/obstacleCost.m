a10c8281204216e6a077794504210f34b2cfcf4f
Marcus Greer <marcusgreer96@gmail.com>
2019-02-19 16:31:35 -0500

    obstacleCost now returns a scalar

--- assn1/obstacleCost.m
+++ assn1/obstacleCost.m
@@ -3,18 +3,18 @@ function [cost] = obstacleCost(linkPoints,obstacles)
 % given a matrix of obstables where each obstacle is a row of x y z radius (spheres)
 % get the distance to all link points (returned by the fk function)
 
-%TODO: make distance from middle of links instead of edges
+%TODO: make distance from middle of links instead of endpoints
 N = size(linkPoints,1);
 M = size(obstacles,1);
 
 obstaclePos = obstacles(:, 1:end-1);
 obstacleRad = obstacles(:, end);
 
-cost = zeros(N,M);
+cost = 0;
 for n = 1:N
     for m = 1:M
         dist = sqrt(sum((linkPoints(n,:) - obstaclePos(m, :)).^2));
-        cost(n,m) = dist - obstacleRad(m);
+        cost = cost + dist - obstacleRad(m);
     end
 end
 
