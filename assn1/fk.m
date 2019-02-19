a5feb09af257c996f69bfcb202d1b1ce8fa5b2e9
seyserkoze <skhazanc@andrew.cmu.edu>
2019-02-19 18:38:19 -0500

    removed error on compile

--- assn1/fk.m
+++ assn1/fk.m
@@ -2,8 +2,6 @@ function [joint_positions] = fk( link_lengths, joint_angles )
 
 joint_positions = zeros(3, numel(link_lengths) + 1);
 
-rotations
-
 for link = size(link_lengths, 2) : -1 : 1
     % get a rotation matrix from the joint (Euler) angles     
     rotation = eul2rotm(joint_angles(:, link)');
