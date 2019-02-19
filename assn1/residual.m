6ff37a25186b9830035223ab973dda4ad69b62f7
Ryan Brigden <rbrigden@cmu.edu>
2019-02-19 16:28:17 -0500

    Add obs plot

--- assn1/residual.m
+++ assn1/residual.m
@@ -1,4 +1,11 @@
 function [position_error] = residual(joint_positions, x)
-    delta_position = joint_positions(:, end) - x(1:3);
+    x_d = x(1:3);
+    x_q = x(3:7);
+
+    % Euclidean distance between target and current effector end     
+    delta_position = joint_positions(:, end) - x_d;
+    
+    % Angular difference between 
+    
     position_error = delta_position;
 end
--- assn1/run.m
+++ assn1/run.m
@@ -13,6 +13,14 @@ x_q = [ 1 0 0 0 ].';
 % Overall target
 x = [ x_d ; x_q ];
 
+% Define obstables (x, y, z, r)
+num_obstacles = 3;
+obs = zeros(4, num_obstacles);
+obs(:, 1) = [0.5 0.5 0.5 0.1].';
+obs(:, 2) = [0.3 0.2 0.4 0.2].';
+obs(:, 3) = [2 2 1 0.5].';
+
+
 % Joint angle limits
 min_roll = (-pi / 2) * ones(1, num_links);
 max_roll = (pi / 2) * ones(1, num_links);
@@ -27,8 +35,8 @@ lower_bounds = [ min_yaw; min_pitch; min_roll ];
 upper_bounds = [ max_yaw; max_pitch; max_roll ];
 
 % Optimize joint angles (Just joint position. No rotation or obstacles yet).
-[final_angles] = optim(x, link_lengths, lower_bounds, upper_bounds);
+[final_angles] = optim(x, link_lengths, obs, lower_bounds, upper_bounds);
 
 % Do the forward kinematics again
 [joint_positions] = fk(link_lengths, final_angles);
-draw3(joint_positions);
+draw3(joint_positions, obs);
